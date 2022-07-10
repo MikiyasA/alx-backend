655bf9bad89b39a786c35bc9247a73e1ba957e5c
Mikiyas Adere <mikiyasad86@gmail.com>
2022-07-10 13:18:29 -0700

    To fix some errors

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,7 +8,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple:
     """ return a tuple of size two containing a start
     index and an end index"""
-    start_index  = ((page - 1) * page_size)
+    start_index = ((page - 1) * page_size)
     end_index = page_size * page
-        
+
     return (start_index, end_index)
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-method named get_page that takes two integer arguments page with default value 1
-and page_size with default value 10
+method named get_page that takes two integer arguments page with default
+value 1 and page_size with default value 10
 """
 from typing import Tuple
 import csv
@@ -12,9 +12,9 @@ from typing import List
 def index_range(page: int, page_size: int) -> Tuple:
     """ return a tuple of size two containing a start
     index and an end index"""
-    start_index  = ((page - 1) * page_size)
+    start_index = ((page - 1) * page_size)
     end_index = page_size * page
-        
+
     return (start_index, end_index)
 
 
@@ -38,7 +38,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ Use assert to verify that both arguments are integers greater than 0
+        """ Use assert to verify that both arguments are integers greater
+        than 0
         If the input arguments are out of range for the dataset, an empty list
         should be returned.
         """
@@ -46,7 +47,7 @@ class Server:
         assert isinstance(page_size, int)
         assert page > 0
         assert page_size > 0
-        
+
         p, s = index_range(page, page_size)
-        
+
         return self.__dataset[p:s]
