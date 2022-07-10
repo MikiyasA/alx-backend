c660f36a28bac6ab6eb7039dcd761a7431044906
Mikiyas Adere <mikiyasad86@gmail.com>
2022-07-10 13:03:34 -0700

    To check task 0 & 1

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes two integer arguments page and page_size
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """ return a tuple of size two containing a start
+    index and an end index"""
+    start_index  = ((page - 1) * page_size)
+    end_index = page_size * page
+        
+    return (start_index, end_index)
